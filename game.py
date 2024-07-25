d467c7ca493860ab9585ce87ce2b44948253a29c
AqibAamir <144609363+AqibAamir@users.noreply.github.com>
2024-07-25 20:06:48 +0100

    finish game.py

--- game.py
+++ game.py
@@ -26,3 +26,34 @@ def determine_winner(player, computer):
         if computer == 'rock':
             return "You win!"
         else:
+            return "You lose!"
+    elif player == 'scissors':
+        if computer == 'paper':
+            return "You win!"
+        else:
+            return "You lose!"
+
+def play_again():
+    while True:
+        play_again_choice = input("Do you want to play again? (yes/no): ").lower()
+        if play_again_choice in ('yes', 'no'):
+            return play_again_choice == 'yes'
+        else:
+            print("Invalid choice, please enter 'yes' or 'no'.")
+
+def play_game():
+    while True:
+        computer_choice = get_computer_choice()
+        player_choice = get_player_choice()
+        
+        print(f"Computer chose: {computer_choice}")
+        
+        result = determine_winner(player_choice, computer_choice)
+        print(result)
+        
+        if not play_again():
+            break
+
+play_game()
+
+
